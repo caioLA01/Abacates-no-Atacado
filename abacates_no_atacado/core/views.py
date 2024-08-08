bc8b3b8087d64f015c13bef7ea97214b5247276d
AllanRPereira <allanflyk2@gmail.com>
2024-08-08 14:50:25 -0300

    Index

--- abacates_no_atacado/core/views.py
+++ abacates_no_atacado/core/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from pprint import pp
 
 # Create your views here.
 
 def home(request):
     return render(request, "core/index.html")
+
+def analyzer(request):
+    context = {
+        
+    }
+
+    return render(request, "core/index.html", context)
+
+
+def check_file_extensions(files):
+    files["model"]
