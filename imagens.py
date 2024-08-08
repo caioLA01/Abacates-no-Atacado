d83fa94e9a8e9e55180664d9dd5cbbf3986e9fc5
AllanRPereira <allanflyk2@gmail.com>
2024-08-08 11:54:32 -0300

    Análisei magens funcionando

--- imagens.py
+++ imagens.py
@@ -1,5 +1,7 @@
 import base64
 import os
+import json
+import hashlib
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -14,6 +16,9 @@ def encode_image(image_path):
 dir = "images"
 base64_images = []
 for file in os.listdir(dir):
+    if ".json" in file:
+       continue
+
     base64_image = encode_image(f"{dir}/{file}")
     base64_images.append(base64_image)
 
@@ -21,7 +26,7 @@ for file in os.listdir(dir):
 content = [
    {
     "type": "text",
-    "text": "What are in these images? Is there any difference between them?",
+    "text": "Can you understand this images ?"
    }
 ]
 
@@ -30,21 +35,38 @@ for image in base64_images:
     image_data_send = {
         "type": "image_url",
         "image_url": {
-        "url" : f"data:image/jpeg;base64,{image}"
+        "url" : f"data:image/jpeg;base64,{image}",
+        "detailt" : "high"
         }
     }
     content.append(image_data_send)
+
+with open(f"{dir}/asset_info.json") as file:
+   machine_data_json = json.load(file)
 
 client = OpenAI()
 response = client.chat.completions.create(
   model="gpt-4o",
   messages=[
+    #{
+     # "role" : "system",
+      #"content" : f"You are an engineer who needs to report \
+       #   machine specifications to a team of mechanics and you have this information, in json, about the machine: {machine_data_json}" 
+    #},
     {
       "role": "user",
       "content": content
-    }
+    },
   ],
   max_tokens=500,
 )
 
-print(response)
+
+random_bytes = os.urandom(16)  # 16 bytes of random data
+
+# Create a SHA-256 hash of the random bytes
+hash_object = hashlib.sha256(random_bytes)
+hash_hex = hash_object.hexdigest()
+
+with open(f"respostas/resposta_{hash_hex}.txt", "w+") as resposta:
+   resposta.write(str(response.choices[0].message))
